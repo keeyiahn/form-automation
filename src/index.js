d6bdf44e63d896190bf2cb24e73d967b721ab106
kee <notthad@gmail.com>
2025-10-16 13:11:39 +0800

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+const Telenode = require("telenode-js");
 const puppeteer = require('puppeteer');
 const moment = require('moment-timezone');
 
+const bot = new Telenode({
+  apiToken: process.env.API_TOKEN
+});
 const url = process.env.URL;
 const name = process.env.NAME;
 const radio = "Check in";
+const channel = process.env.CHAT_ID;
 
-async function fillForm() {
+async function main() {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
@@ -34,7 +39,7 @@ async function fillForm() {
   const dateInput = await page.$("input[aria-label='Date picker']");
   const placeholder = await page.evaluate(el => el.placeholder, dateInput);
   console.log(placeholder);
-  
+
   let date;
   if (placeholder.includes("dd/MM")) {
     // Singapore-style
@@ -43,8 +48,8 @@ async function fillForm() {
     // US-style
     date = moment().tz("Asia/Singapore").format("MM/DD/YYYY");
   }
-  
   console.log("Date: " + date);
+
   // Fill date input
   await page.type("input[aria-label='Date picker']", date);
 
@@ -52,8 +57,12 @@ async function fillForm() {
   await page.click("button[data-automation-id='submitButton']");
   console.log(`Form submitted`);
 
-  await new Promise(resolve => setTimeout(resolve, 3000)); // wait for 3 seconds
+  await new Promise(resolve => setTimeout(resolve, 3000)); 
   await browser.close();
+
+  notif = `Submitting check-in form... \nName: ${name} \nRadio: ${radio} \nDate: ${date}`;
+
+  await bot.sendTextMessage(notif, channel);
 }
 
-fillForm();
+main();
